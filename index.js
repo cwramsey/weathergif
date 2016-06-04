7a4c4beccc161b7ec8487f5f68d2299d35b3ec45
chrisramsey <cramsey@helpsocial.com>
2016-06-03 21:52:30 -0500

    Adds cors options

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin: /^[^.\s]+\.mixmax\.com$/,
+  credentials: true
+}));
 
 app.set('port', (process.env.PORT || 5000));
 
