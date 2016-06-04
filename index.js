c7cff93ba0577d85b7cc6dff1e64a1f619d34d40
chrisramsey <cramsey@helpsocial.com>
2016-06-03 23:02:43 -0500

    Adds typeahead and resolver

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+'use strict';
+
+require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
-app.set('port', (process.env.PORT || 5000));
-
-const corsOpts = {
+const corsOptions = {
   origin: /^[^.\s]+\.mixmax\.com$/,
   credentials: true
 };
 
-app.get('/typeahead', cors(corsOpts), require('./lib/typeahead'));
+app.get('/typeahead', cors(corsOptions), require('./lib/typeahead'));
+app.get('/resolver', cors(corsOptions), require('./lib/resolver'));
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(process.env.PORT || 9145);
