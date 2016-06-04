e9cd634c5b1980f10e6a1a69020dfbed3a3d99b8
chrisramsey <cramsey@helpsocial.com>
2016-06-03 21:49:51 -0500

    Adds typeahead

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/typeahead', require('./lib/typeahead'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
